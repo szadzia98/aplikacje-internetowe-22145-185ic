3b4e907a621382c1c141a55e913e63910a9771ce
szadzia98 <szadzia98@gmail.com>
2020-11-13 21:01:10 +0100

    Lab 1

--- Lab1/README.md
+++ Lab1/README.md
@@ -1,1 +1,17 @@
 # Lab 1
+## Tworzenie blogu na heroku
+Widok główny blogu z postami  
+Plus w prawym górnym rogu służy do dodawania nowego postu  
+![](Screen/1.jpg)
+Widok po wejściu w post  
+Dwa przyciski na stronie:  
+Przycisk z ołówkiem służy do edycji posta  
+Przycisk z "x" służy do usuwania posta
+![](Screen/2.jpg)
+Widok dodawania nowego postu  
+![](Screen/3.jpg)
+Widok edycji postu  
+![](Screen/4.jpg)  
+Bez zalogowania nie można dodawać/edytować/usuwać postów  
+Link do blogu  
+https://aplikacje-internetowe-22145.herokuapp.com/
--- Lab1/myblog/settings.py
+++ Lab1/myblog/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
-import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm2nqb!0vwhk74!wher$rstc+qqz@cuwq)ngz^)z%aw^uqlh@v0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myblog.urls'
@@ -76,12 +77,8 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'srodowisko',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -123,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+django_heroku.settings(locals())
