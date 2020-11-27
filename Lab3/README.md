70274e09d8a35d890c6d870c551b495b3589a11d
szadzia98 <szadzia98@gmail.com>
2020-11-27 11:44:42 +0100

    Lab 3

---
+++ Lab3/README.md
@@ -0,0 +1,15 @@
+# Lab 1
+## Modyfikacja blogu 
+### Dodanie uwierzytelniania za pomocą social media
+W folderze **Zmienione pliki** znajdują się tylko pliki zmienione   
+__Reszta plików jest taka sama jak w folderze Lab2__  
+Dodane backendy
+Github oraz Facebook
+Widok przy logowaniu/ umożliwienie logowania przez social media
+![](Screen/1.jpg)  
+Uwierzytelnienie przez Facebook (z uwierzyteniem przez Github jest taka sama procedura)  
+![](Screen/2.jpg)  
+Przejście do strony dashboard po zalogowaniu przez social media (w tym wypadku Facebook).  
+![](Screen/3.jpg)  
+Link do blogu  
+https://aplikacje-internetowe-22145.herokuapp.com/
---
+++ Lab3/Zmienione pliki/blog-urls.py
@@ -0,0 +1,22 @@
+from django.urls import include ,path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout'),
+    path('dashboard/',views.dashboard, name = 'dashboard'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+
+]
